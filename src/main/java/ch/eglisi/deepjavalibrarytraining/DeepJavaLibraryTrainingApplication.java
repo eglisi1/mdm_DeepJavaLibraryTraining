db742f97758db4e71010d05d8e1b69bdb9f9a4a3
Simon Egli <eglisi1@students.zhaw.ch>
2023-04-11 08:49:56 +0200

    create project with SpringBoot initializr

---
+++ src/main/java/ch/eglisi/deepjavalibrarytraining/DeepJavaLibraryTrainingApplication.java
@@ -0,0 +1,13 @@
+package ch.eglisi.deepjavalibrarytraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepJavaLibraryTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeepJavaLibraryTrainingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/eglisi/deepjavalibrarytraining/DeepJavaLibraryTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.eglisi.deepjavalibrarytraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepJavaLibraryTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
